67c23377d813c5e3badf57e50e8f8f8f5fe6ba15
Redzwhite <redzwhite9997@gmail.com>
2022-09-04 15:40:06 +0300

    HomeWork3

---
+++ src/main/java/com/hillel/livinsky/lessons/lesson3/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.livinsky.lessons.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int oneSide = 10;
+        int secondSide = 10;
+        int height = 15;
+        int volume = oneSide * secondSide * height;
+
+        System.out.println(volume  + " volume");
+    }
+}
