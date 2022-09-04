e587a4c2f356ee95df0d2e2556cff5bbcb6b15e0
Redzwhite <redzwhite9997@gmail.com>
2022-09-04 16:39:41 +0300

    Homework3

---
+++ src/main/java/com/hillel/livinsky/lessons/lesson2/Main.java
@@ -0,0 +1,18 @@
+package com.hillel.livinsky.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String objectName = "Eiffel Tower";
+        char coordinateSymbol = '\u00B0';
+        double latitude = 48.8582573;
+        double longitude = 2.295258;
+        System.out.println(objectName + " coordinates" + coordinateSymbol + ": " + latitude + " northern latitude and " + longitude + " east longitude.");
+
+
+
+
+
+
+    }
+}
