bb656cb7e87e0213e8d9634601934c0d38fae049
Redzwhite <redzwhite9997@gmail.com>
2022-09-13 18:02:46 +0300

    bag fixes in homework6

--- src/main/java/com/hillel/livinsky/lessons/lesson5/Main.java
+++ src/main/java/com/hillel/livinsky/lessons/lesson5/Main.java
@@ -5,123 +5,114 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner firstNameForTeam = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter a name for first team");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a team name for the first team");
+        String firstNameForTeam = new String();
+        firstNameForTeam = scanner.nextLine();
+        System.out.println("Team name for the fist team is: " + firstNameForTeam);
+        System.out.println("Enter a team name for the second team");
+        String secondNameForTeam = new String();
+        secondNameForTeam = scanner.nextLine();
+        if (firstNameForTeam.equals(secondNameForTeam)) {
+            System.out.println("Team name already exists, Restart the program and try again!");
+            System.exit(0);
+        } else {
+            System.out.println("Team name for the second team is: " + secondNameForTeam);
+        }
 
-        String teamNameOne = firstNameForTeam.nextLine();
-        System.out.println("Name for fist team is: " + teamNameOne);
-
-        Scanner secondNameForTeam = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter a name for second team");
-
-        String teamNameTwo = secondNameForTeam.nextLine();
-        System.out.println("Name for second team is: " + teamNameTwo);
-
-        Scanner fragsPlayerOneTeamOne = new Scanner(System.in);
-        System.out.println("Enter number of frags for player one per team " + teamNameOne);
-        if (fragsPlayerOneTeamOne.hasNextInt()) {
-            fragsPlayerOneTeamOne.nextInt();
+        System.out.println("Enter number of frags for player 1 per team " + firstNameForTeam);
+        int fragsPlayerOneTeamOne = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerOneTeamOne = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 2 per team " + firstNameForTeam);
+        int fragsPlayerTwoTeamOne = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerTwoTeamOne = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 3 per team " + firstNameForTeam);
+        int fragsPlayerThreeTeamOne = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerThreeTeamOne = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 4 per team " + firstNameForTeam);
+        int fragsPlayerFourTeamOne = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerFourTeamOne = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 5 per team " + firstNameForTeam);
+        int fragsPlayerFiveTeamOne = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerFiveTeamOne = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 1 per team " + secondNameForTeam);
+        int fragsPlayerOneTeamTwo = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerOneTeamTwo = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 2 per team " + secondNameForTeam);
+        int fragsPlayerTwoTeamTwo = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerTwoTeamTwo = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 3 per team " + secondNameForTeam);
+        int fragsPlayerThreeTeamTwo = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerThreeTeamTwo = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 4 per team " + secondNameForTeam);
+        int fragsPlayerFourTeamTwo = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerFourTeamTwo = scanner.nextInt();
+        } else {
+            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
+            System.exit(0);
+        }
+        System.out.println("Enter number of frags for player 5 per team " + secondNameForTeam);
+        int fragsPlayerFiveTeamTwo = 0;
+        if (scanner.hasNextInt()) {
+            fragsPlayerFiveTeamTwo = scanner.nextInt();
         } else {
             System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
             System.exit(0);
         }
 
-        Scanner fragsPlayerTwoTeamOne = new Scanner(System.in);
-        System.out.println("Enter number of frags for player two per team " + teamNameOne);
-        if (fragsPlayerTwoTeamOne.hasNextInt()) {
-            fragsPlayerTwoTeamOne.nextInt();
+        int averageSumFragsTeamOne = (fragsPlayerOneTeamOne + fragsPlayerTwoTeamOne + fragsPlayerThreeTeamOne + fragsPlayerFourTeamOne + fragsPlayerFiveTeamOne) / 5;
+        int averageSumFragsTeamTwo = (fragsPlayerOneTeamTwo + fragsPlayerTwoTeamTwo + fragsPlayerThreeTeamTwo + fragsPlayerFourTeamTwo + fragsPlayerFiveTeamTwo) / 5;
+        if (averageSumFragsTeamOne > averageSumFragsTeamTwo) {
+            System.out.println("Перемогла команда " + firstNameForTeam + " набрала " + averageSumFragsTeamOne + " очків");
+        } else if (averageSumFragsTeamOne < averageSumFragsTeamTwo) {
+            System.out.println("Перемогла команда " + secondNameForTeam + " набрала " + averageSumFragsTeamTwo + " очків");
         } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
+            System.out.println("Нічия " + firstNameForTeam + " набрала " + averageSumFragsTeamOne + " очків та " + secondNameForTeam + " набрала " + averageSumFragsTeamTwo + " очків");
+
         }
-
-        Scanner fragsPlayerThreeTeamOne = new Scanner(System.in);
-        System.out.println("Enter number of frags for player three per team " + teamNameOne);
-        if (fragsPlayerThreeTeamOne.hasNextInt()) {
-            fragsPlayerThreeTeamOne.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerFourTeamOne = new Scanner(System.in);
-        System.out.println("Enter number of frags for player four per team " + teamNameOne);
-        if (fragsPlayerFourTeamOne.hasNextInt()) {
-            fragsPlayerFourTeamOne.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerFiveTeamOne = new Scanner(System.in);
-        System.out.println("Enter number of frags for player five per team " + teamNameOne);
-        if (fragsPlayerFiveTeamOne.hasNextInt()) {
-            fragsPlayerFiveTeamOne.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerOneTeamTwo = new Scanner(System.in);
-        System.out.println("Enter number of frags for player one per team " + teamNameTwo);
-        if (fragsPlayerOneTeamTwo.hasNextInt()) {
-            fragsPlayerOneTeamTwo.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerTwoTeamTwo = new Scanner(System.in);
-        System.out.println("Enter number of frags for player two per team " + teamNameTwo);
-        if (fragsPlayerTwoTeamTwo.hasNextInt()) {
-            fragsPlayerTwoTeamTwo.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerThreeTeamTwo = new Scanner(System.in);
-        System.out.println("Enter number of frags for player three per team " + teamNameTwo);
-        if (fragsPlayerThreeTeamTwo.hasNextInt()) {
-            fragsPlayerThreeTeamTwo.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerFourTeamTwo = new Scanner(System.in);
-        System.out.println("Enter number of frags for player four per team " + teamNameTwo);
-        if (fragsPlayerFourTeamTwo.hasNextInt()) {
-            fragsPlayerFourTeamTwo.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-        Scanner fragsPlayerFiveTeamTwo = new Scanner(System.in);
-        System.out.println("Enter number of frags for player five per team " + teamNameTwo);
-        if (fragsPlayerFiveTeamTwo.hasNextInt()) {
-            fragsPlayerFiveTeamTwo.nextInt();
-        } else {
-            System.out.println("INCORRECT DATA! RESTART THE PROGRAM AND TRY AGAIN");
-            System.exit(0);
-        }
-
-
-        int averageFragsTeamOne = (fragsPlayerOneTeamOne.nextInt() + fragsPlayerTwoTeamOne.nextInt() + fragsPlayerThreeTeamOne.nextInt() + fragsPlayerFourTeamOne.nextInt() + fragsPlayerFiveTeamOne.nextInt()) / 5;
-        int averageFragsTeamTwo = (fragsPlayerOneTeamTwo.nextInt() + fragsPlayerTwoTeamTwo.nextInt() + fragsPlayerThreeTeamTwo.nextInt() + fragsPlayerFourTeamTwo.nextInt() + fragsPlayerFiveTeamTwo.nextInt()) / 5;
-        if (averageFragsTeamOne > averageFragsTeamTwo) {
-            System.out.println("Перемогла команда " + teamNameOne + " набрала " + averageFragsTeamOne + " очків");
-        } else if (averageFragsTeamOne < averageFragsTeamTwo) {
-            System.out.println("Перемогла команда " + teamNameTwo + " набрала " + averageFragsTeamTwo + " очків");
-        } else {
-            System.out.println("Нічия " + teamNameOne + " набрала " + averageFragsTeamOne + " очків та " + teamNameTwo + " набрала " + averageFragsTeamTwo + " очків");
-        }
-
     }
-
 }
-
 
 
 
